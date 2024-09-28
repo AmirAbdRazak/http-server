d629fd6ee6724a69c542a90b95fc76f7126b140f
Amir Razak <amrrzk02@gmail.com>
2024-09-28 15:11:39 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #[allow(unused_imports)]
 use std::net::TcpListener;
 
+enum StatusCode {
+    OK,
+}
+
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
@@ -10,7 +14,7 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
-                println!("accepted new connection");
+                println!("HTTP/1.1 200 OK\r\n\r\n");
             }
             Err(e) => {
                 println!("error: {}", e);
