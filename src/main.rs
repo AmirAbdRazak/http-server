245a64338260e4e39a77a5f9060f02e4e28090b9
Amir Razak <amrrzk02@gmail.com>
2024-09-28 23:41:53 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,29 @@ enum HttpVersion {
     Http1_1,
 }
 
+enum ContentType {
+    PlainText,
+}
+
 struct Response {
     http_version: HttpVersion,
     status_code: StatusCode,
-    //body
+    headers: Vec<String>,
+    body: String,
+}
+
+impl Response {
+    fn add_header(&mut self, header_name: &str, header_value: &str) {
+        let header = format!("{header_name}: {header_value}");
+        self.headers.push(header);
+    }
+}
+
+struct Request {
+    http_method: HttpMethod,
+    request_target: String,
+    http_version: HttpVersion,
+    headers: HashMap<String, String>,
 }
 
 impl fmt::Display for StatusCode {
@@ -35,6 +54,13 @@ impl fmt::Display for HttpVersion {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             HttpVersion::Http1_1 => write!(f, "HTTP/1.1"),
+        }
+    }
+}
+impl fmt::Display for ContentType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            ContentType::PlainText => write!(f, "text/plain"),
         }
     }
 }
@@ -54,10 +80,16 @@ impl fmt::Display for HttpException {
     }
 }
 
+// HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\nabc
 impl fmt::Display for Response {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let crlf = "\r\n\r\n";
-        write!(f, "{} {}{}", self.http_version, self.status_code, crlf)
+        let crlf = "\r\n";
+        let concatenated_header = self.headers.join(crlf) + crlf;
+        write!(
+            f,
+            "{} {}{}{}{}{}",
+            self.http_version, self.status_code, crlf, concatenated_header, crlf, self.body
+        )
     }
 }
 
@@ -90,23 +122,42 @@ impl HttpVersion {
     }
 }
 
-struct Request {
-    http_method: HttpMethod,
-    request_target: String,
-    http_version: HttpVersion,
-    headers: HashMap<String, String>,
-}
-
 fn handle_client(request: Request) -> Response {
-    match request.request_target.as_str() {
-        "/" => Response {
+    let request_path_vec: Vec<_> = request
+        .request_target
+        .split("/")
+        .filter(|path_section| path_section.len() > 0)
+        .collect();
+
+    if request_path_vec.len() == 0 {
+        Response {
             http_version: HttpVersion::Http1_1,
             status_code: StatusCode::Ok,
-        },
-        _ => Response {
+            body: String::from(""),
+            headers: vec![],
+        }
+    } else if request_path_vec.len() == 2 && request_path_vec[0] == "echo" {
+        let mut response = Response {
+            http_version: HttpVersion::Http1_1,
+            status_code: StatusCode::Ok,
+            body: String::from(request_path_vec[1]),
+            headers: vec![],
+        };
+
+        response.add_header("Content-Type", &ContentType::PlainText.to_string());
+        response.add_header(
+            "Content-Length",
+            &response.body.as_bytes().len().to_string(),
+        );
+
+        response
+    } else {
+        Response {
             http_version: HttpVersion::Http1_1,
             status_code: StatusCode::NotFound,
-        },
+            body: String::from(""),
+            headers: vec![],
+        }
     }
 }
 
@@ -144,16 +195,15 @@ fn parse_request(buf_reader: BufReader<&mut TcpStream>) -> Result<Request, HttpE
 }
 
 // GET /index.html HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+// HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\nabc
 
+fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                println!("accepted the connection");
+                println!("=== Connection Established! ===");
                 let buf_reader = BufReader::new(&mut stream);
 
                 let request = parse_request(buf_reader);
